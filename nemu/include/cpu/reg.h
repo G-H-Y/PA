4ef84995fd7d384cc7e2983a9c06bbb41e8ca62b
tracer-ics2018 <tracer@njuics.org>
2018-12-21 09:25:49 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:25:49 up  1:05,  1 user,  load average: 1.78, 1.38, 1.03 a3517844e939293bfece7be67af37d2fea46f95c

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,8 +28,10 @@ typedef struct {
     uint32_t _32;
     uint16_t _16;
     uint8_t _8[2];
-  } gpr[8];
+    } gpr[8];
+  struct{
     rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    };  
   };
 
   vaddr_t eip;
