6b040aeef506d4187849588c904da474f7c3744d
tracer-ics2018 <tracer@njuics.org>
2018-12-24 11:09:34 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:09:34 up  1:53,  1 user,  load average: 1.07, 1.00, 0.81 b4371069e10aa7d32f3bd112303a33262a55725

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -101,15 +101,15 @@ static int cmd_info(char *args){
     return 0;
   }
   else if(strcmp(arg,"r")==0){
-    printf("eax\t%04x0x\n",cpu.eax);
-    printf("ebp\t%04x0x\n",cpu.ebp);
-    printf("ebx\t%04x0x\n",cpu.ebx);
-    printf("ecx\t%04x0x\n",cpu.ecx);
-    printf("edi\t%04x0x\n",cpu.edi);
-    printf("edx\t%04x0x\n",cpu.edx);
-    printf("eip\t%04x0x\n",cpu.eip);
-    printf("esi\t%04x0x\n",cpu.esi);
-    printf("esp\t%04x0x\n",cpu.esp);
+    printf("eax\t0x%04x\n",cpu.eax);
+    printf("ebp\t0x%04x\n",cpu.ebp);
+    printf("ebx\t0x%04x\n",cpu.ebx);
+    printf("ecx\t0x%04x\n",cpu.ecx);
+    printf("edi\t0x%04x\n",cpu.edi);
+    printf("edx\t0x%04x\n",cpu.edx);
+    printf("eip\t0x%04x\n",cpu.eip);
+    printf("esi\t0x%04x\n",cpu.esi);
+    printf("esp\t0x%04x\n",cpu.esp);
   }
   return 0;
 }
