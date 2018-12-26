297687a1ea42cf5159bb044fb946b06d30237aa5
tracer-ics2018 <tracer@njuics.org>
2018-12-26 21:45:02 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  21:45:02 up 20 min,  1 user,  load average: 0.72, 0.49, 0.45 b0fca8113a8ed09cc698467889dee47f546f3a8f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -88,7 +88,7 @@ static bool make_token(char *e)
   regmatch_t pmatch;
 
   nr_token = 0;
-
+  printf("e.length = %ld\n",sizeof(e));
   while (e[position] != '\0')
   {
     /* Try all rules one by one. */
