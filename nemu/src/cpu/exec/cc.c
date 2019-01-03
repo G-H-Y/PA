c7c275525486047451666d11b405c451865d6050
tracer-ics2018 <tracer@njuics.org>
2019-01-03 22:55:43 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:55:43 up  2:15,  1 user,  load average: 0.86, 0.64, 0.59 973b4f2c94fe18d52ecb4292fde8a428c82875b

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -40,8 +40,13 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       //printf("dest = %d\n",t0==t1);
       break;
     case CC_LE:
-    printf("CC_LE\n");
-      TODO();
+      rtl_get_ZF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xori(&t0,&t0,0x1);
+      rtl_xor(&t3,&t1,&t2);
+      rtl_or(dest,&t0,&t3);
+      break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
