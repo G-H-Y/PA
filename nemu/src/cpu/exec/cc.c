2c12955ca39dc0bd9be1eff91eda028c7198622c
tracer-ics2018 <tracer@njuics.org>
2019-01-03 23:56:29 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:56:29 up  3:16,  1 user,  load average: 1.50, 0.66, 0.46 28b8d1eef053cb7a427573bbfff5b20e5dd5bbbb

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -20,28 +20,28 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
       printf("CC_0 CC_B\n");
       TODO();
     case CC_E:
-      rtl_get_ZF(&t0);
-      *dest = (t0 == 1);
+      rtl_get_ZF(&t1);
+      *dest = (t1 == 1);
       break;
     case CC_NE:
-      rtl_get_ZF(&t0);
-      *dest = (t0 == 0);
+      rtl_get_ZF(&t1);
+      *dest = (t1 == 0);
       break;
     case CC_BE:
     case CC_S:
     TODO();
     case CC_L:
-      rtl_get_SF(&t0);
-      rtl_get_OF(&t1);
-      printf("in cc SF/t0 = %d,OF/t1 = %d\n",t0,t1);
-      *dest = (t0 == t1);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      printf("in cc SF/t1 = %d,OF/t2 = %d\n",t1,t2);
+      *dest = (t1 == t2);
       printf("dest = %d\n",*dest);
       break;
     case CC_LE:
-      rtl_get_ZF(&t0);
-      rtl_get_SF(&t1);
-      rtl_get_OF(&t2);
-      *dest = ((t0 == 1) || (t1 != t2));
+      rtl_get_ZF(&t1);
+      rtl_get_SF(&t2);
+      rtl_get_OF(&t3);
+      *dest = ((t1 == 1) || (t2 != t3));
       break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
