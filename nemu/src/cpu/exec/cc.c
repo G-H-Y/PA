04d18136641c16ed8911d0c11c4043b55ddea130
tracer-ics2018 <tracer@njuics.org>
2019-01-03 22:23:12 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:23:12 up  1:42,  1 user,  load average: 0.13, 0.40, 0.49 ccf397d0167482ac99fc51e962792cf6d4ed3fc2

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -13,6 +13,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
 
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
+  printf("subcode=%x\n",subcode);
   switch (subcode & 0xe) {
     case CC_O:
     case CC_B:
