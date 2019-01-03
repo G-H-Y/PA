f1ecf91ae367aba2f06c465e5950b0b5e787289a
tracer-ics2018 <tracer@njuics.org>
2019-01-03 14:30:56 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:30:56 up  6:21,  1 user,  load average: 0.41, 0.63, 0.63 435f0929426bf8cdac0333fb9e7793c44fadb0f0

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -293,6 +293,7 @@ make_DHelper(J) {
   decode_op_SI(eip, id_dest, false);
   // the target address can be computed in the decode stage
   decoding.jmp_eip = id_dest->simm + *eip;
+  printf("simm = %x\tjmp_eip = %x\n",id_dest->simm,decoding.jmp_eip);
 }
 
 make_DHelper(push_SI) {
