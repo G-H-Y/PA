7b06c5191e5298f0e5d6720cc73500c6210aa8ec
tracer-ics2018 <tracer@njuics.org>
2019-01-04 11:52:11 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:52:11 up  3:29,  1 user,  load average: 0.95, 0.55, 0.53 1b0b6685218e79e0c73562aaf65ea649e44335f6

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -165,6 +165,7 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   uint32_t sign = (*src1) >> ((width << 3)-1);
   if(sign) *dest = (*src1) | (0xffffffff << (width << 3));
   else *dest = (*src1) & (~(0xffffffff << (width << 3)));
+  printf("in sext:(~(0xffffffff << (width << 3))) = %x\n",(~(0xffffffff << (width << 3))));
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
