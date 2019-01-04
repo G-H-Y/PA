683bac1063e03cfcd49920c2e5b1e97fb9e82acc
tracer-ics2018 <tracer@njuics.org>
2019-01-04 11:27:37 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  11:27:37 up  3:05,  1 user,  load average: 0.51, 0.60, 0.51 b0922bfd56e904639cfccf2becda107a55a49dd1

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -76,7 +76,9 @@ make_EHelper(cwtl) {
 
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
+  printf("in movsx: src = %d\n",id_src->val);
   rtl_sext(&t0, &id_src->val, id_src->width);
+  printf("in movsx: res/t0 = %d\n",t0);
   operand_write(id_dest, &t0);
   print_asm_template2(movsx);
 }
