ca50da9d7278b124044a5794b608eb7c97dc0a5c
tracer-ics2018 <tracer@njuics.org>
2019-01-04 09:14:12 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  09:14:12 up 51 min,  1 user,  load average: 0.78, 0.73, 0.65 fd22c22d5c253238d5808bed87826559facb71f6

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,17 +2,12 @@
 
 make_EHelper(add) {
   rtl_add(&t0,&id_dest->val,&id_src->val);
-  printf("in add: t0 = %d,dest = %d,src = %d\n",t0,id_dest->val,id_src->val);
-  printf("before: eax = %d\n",cpu.eax);
   operand_write(id_dest,&t0);
-  printf("after: eax = %d\n",cpu.eax);
+  
   rtl_update_ZFSF(&t0,id_dest->width);
 
-  printf("afterZFSF:t0 = %d",t0);
   rtl_setrelop(RELOP_LTU,&t1,&t0,&id_dest->val);
-  
   rtl_set_CF(&t1);
-  printf("in add: CF = %d\n",t1);
   
   rtl_xor(&t2,&id_dest->val,&id_src->val);
   rtl_not(&t2,&t2);
