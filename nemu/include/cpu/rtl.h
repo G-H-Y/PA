0efb4b7558fc015e5cab5c281f33e2e4fcc3146e
tracer-ics2018 <tracer@njuics.org>
2019-01-04 10:05:25 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:05:25 up  1:42,  1 user,  load average: 0.58, 0.51, 0.36 5ab13da5e79111659e5129f2f13861dbdbedbb3

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -160,8 +160,11 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-  printf("eip = %x\n",cpu.eip);
-  TODO();
+  //printf("eip = %x\n",cpu.eip);
+  //TODO();
+  uint32_t sign = (*src1) >> ((width << 3)-1);
+  if(sign) *dest = (*src1) | 0xffffffff;
+  else *dest = (*src1) | 0x0;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
