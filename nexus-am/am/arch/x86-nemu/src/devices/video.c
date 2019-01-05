010a3b2efc41567b93ea9eb427de5823ecccf58a
tracer-ics2018 <tracer@njuics.org>
2019-01-05 22:26:48 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:26:48 up  6:17,  1 user,  load average: 0.46, 0.55, 0.55 99caba63123feca148cabe8c2f4d5e2fbe74c64

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -3,33 +3,44 @@
 #include <amdev.h>
 #include <klib.h>
 
-static uint32_t* const fb __attribute__((used)) = (uint32_t *)0x40000;
+static uint32_t *const fb __attribute__((used)) = (uint32_t *)0x40000;
 
-size_t video_read(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_INFO: {
-      _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 0;
-      info->height = 0;
-      return sizeof(_VideoInfoReg);
-    }
+size_t video_read(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_INFO:
+  {
+    _VideoInfoReg *info = (_VideoInfoReg *)buf;
+    info->width = (inl(0x100) >> 16);
+    info->height = (inl(0x100) & 0xff);
+    return sizeof(_VideoInfoReg);
+  }
   }
   return 0;
 }
 
-size_t video_write(uintptr_t reg, void *buf, size_t size) {
-  switch (reg) {
-    case _DEVREG_VIDEO_FBCTL: {
-      _FBCtlReg *ctl = (_FBCtlReg *)buf;
-
-      if (ctl->sync) {
-        // do nothing, hardware syncs.
-      }
-      return sizeof(_FBCtlReg);
+size_t video_write(uintptr_t reg, void *buf, size_t size)
+{
+  switch (reg)
+  {
+  case _DEVREG_VIDEO_FBCTL:
+  {
+    _FBCtlReg *ctl = (_FBCtlReg *)buf;
+    int i;
+    int size = screen_width() * screen_height();
+    for (i = 0; i < size; i++)
+      fb[i] = i;
+    if (ctl->sync)
+    {
+      // do nothing, hardware syncs.
     }
+    return sizeof(_FBCtlReg);
+  }
   }
   return 0;
 }
 
-void vga_init() {
+void vga_init()
+{
 }
