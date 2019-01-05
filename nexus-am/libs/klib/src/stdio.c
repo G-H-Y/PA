ad9a1aa6f1cb3d2c078594acbb73b856e035fd6e
tracer-ics2018 <tracer@njuics.org>
2019-01-05 18:05:00 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  18:05:01 up  1:56,  1 user,  load average: 1.09, 1.48, 1.50 2450eead0b8102ac24cfb3e8bd4a8543c0cc44

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -3,149 +3,152 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-
-uint32_t write_int(char* buffer,int value);
+uint32_t write_int(char *buffer, int value);
 int sprintf(char *out, const char *fmt, ...);
 
-int printf(const char *fmt, ...) {
+int printf(const char *fmt, ...)
+{
   char buf[30];
-  memset(buf,'\0',30);
-  char* out = buf;
-   va_list arg_ptr;
-    uint32_t fmt_length = strlen(fmt);
-    uint32_t index = 0;
-    uint32_t cnt = 0;
-    uint32_t i = 0;
-    uint32_t tmp = 0;
-     
-    int a_int;
-    char a_char;
-    char* a_str;
-     
-    va_start(arg_ptr,fmt);
-    for(index = 0; index < fmt_length; index++)
-    {
-      cnt++;
-      if(fmt[index] != '%')
-        (*out++) = fmt[index];
-      else
-      {
-        switch(fmt[index+1])
-        {
-          case 'd':
-            a_int = va_arg(arg_ptr,int);
-            out += write_int(out,a_int);
-            break;
-          case '0': //02d
-            index++;
-            a_int = va_arg(arg_ptr,int);
-            i = fmt[++index] - '0';
-            tmp = write_int(out,a_int);
-            if(tmp < i){
-              memset(out,'0',i-tmp);
-              out += (i-tmp);
-              out += write_int(out,a_int);
-            }
-            else
-              out += write_int(out,a_int);
-            break;
-          case 's':
-            a_str =(char*)va_arg(arg_ptr,char*);
-            strcpy(out,a_str);
-            out += strlen(a_str);
-            break;
-          case 'c':
-            a_char = va_arg(arg_ptr,int);
-            *(out++) = a_char;
-            break;
-        }
-        index++;
-      }
-    }
-    *out = '\0'; //一定要加！！
-    va_end(arg_ptr);
+  memset(buf, '\0', 30);
+  char *out = buf;
+  va_list arg_ptr;
+  uint32_t fmt_length = strlen(fmt);
+  uint32_t index = 0;
+  uint32_t cnt = 0;
+  uint32_t i = 0;
+  uint32_t tmp = 0;
 
-    for(i = 0; buf[i]!='\0';i++)
-       _putc(buf[i]);
+  int a_int;
+  char a_char;
+  char *a_str;
+
+  va_start(arg_ptr, fmt);
+  for (index = 0; index < fmt_length; index++)
+  {
+    cnt++;
+    if (fmt[index] != '%')
+      (*out++) = fmt[index];
+    else
+    {
+      switch (fmt[index + 1])
+      {
+      case 'd':
+        a_int = va_arg(arg_ptr, int);
+        out += write_int(out, a_int);
+        break;
+      case '0': //02d
+        index++;
+        a_int = va_arg(arg_ptr, int);
+        i = fmt[++index] - '0';
+        tmp = write_int(out, a_int);
+        if (tmp < i)
+        {
+          memset(out, '0', i - tmp);
+          out += (i - tmp);
+          out += write_int(out, a_int);
+        }
+        else
+          out += write_int(out, a_int);
+        break;
+      case 's':
+        a_str = (char *)va_arg(arg_ptr, char *);
+        strcpy(out, a_str);
+        out += strlen(a_str);
+        break;
+      case 'c':
+        a_char = va_arg(arg_ptr, int);
+        *(out++) = a_char;
+        break;
+      }
+      index++;
+    }
+  }
+  *out = '\0'; //一定要加！！
+  va_end(arg_ptr);
+
+  for (i = 0; buf[i] != '\0'; i++)
+    _putc(buf[i]);
   return cnt;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
-}
-
-int snprintf(char *out,size_t n, const char *fmt, ...) {
-  return 0;
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-    va_list arg_ptr;
-    //uint32_t out_length = strlen(out);
-    uint32_t fmt_length = strlen(fmt);
-    uint32_t index = 0;
-    uint32_t cnt = 0;
-     
-    int a_int;
-    char a_char;
-    char* a_str;
-     
-    va_start(arg_ptr,fmt);
-    for(index = 0; index < fmt_length; index++)
-    {
-      cnt++;
-      if(fmt[index] != '%')
-        (*out++) = fmt[index];
-      else
-      {
-        switch(fmt[index+1])
-        {
-          case 'd':
-            a_int = va_arg(arg_ptr,int);
-            out += write_int(out,a_int);
-            break;
-          case 's':
-            a_str =(char*)va_arg(arg_ptr,char*);
-            strcpy(out,a_str);
-            out += strlen(a_str);
-            break;
-          case 'c':
-            a_char = va_arg(arg_ptr,int);
-            *(out++) = a_char;
-            break;
-        }
-        index++;
-      }
-    }
-    *out = '\0'; //一定要加！！
-    va_end(arg_ptr);
-    return cnt;
-}
-
-
-
-uint32_t write_int(char* buffer,int value)
+int vsprintf(char *out, const char *fmt, va_list ap)
 {
-    unsigned char stack[5];
-    uint32_t length = 0;
-    uint32_t tmp = 0;
-    if(value<0){
-        *(buffer++) = '-';
-        value *= -1;
-    }
-    do
-    {
-        stack[length] = '0' + (value % 10);
-        value /= 10;
-        length++;
-    }while(value);
+  return 0;
+}
 
-    tmp = length;
-    while(length)
+int snprintf(char *out, size_t n, const char *fmt, ...)
+{
+  return 0;
+}
+
+int sprintf(char *out, const char *fmt, ...)
+{
+  va_list arg_ptr;
+  //uint32_t out_length = strlen(out);
+  uint32_t fmt_length = strlen(fmt);
+  uint32_t index = 0;
+  uint32_t cnt = 0;
+
+  int a_int;
+  char a_char;
+  char *a_str;
+
+  va_start(arg_ptr, fmt);
+  for (index = 0; index < fmt_length; index++)
+  {
+    cnt++;
+    if (fmt[index] != '%')
+      (*out++) = fmt[index];
+    else
     {
-        *buffer = stack[length-1];
-        length--;
-        buffer++;
+      switch (fmt[index + 1])
+      {
+      case 'd':
+        a_int = va_arg(arg_ptr, int);
+        out += write_int(out, a_int);
+        break;
+      case 's':
+        a_str = (char *)va_arg(arg_ptr, char *);
+        strcpy(out, a_str);
+        out += strlen(a_str);
+        break;
+      case 'c':
+        a_char = va_arg(arg_ptr, int);
+        *(out++) = a_char;
+        break;
+      }
+      index++;
     }
-    return tmp;
+  }
+  *out = '\0'; //一定要加！！
+  va_end(arg_ptr);
+  return cnt;
+}
+
+uint32_t write_int(char *buffer, int value)
+{
+  unsigned char stack[20];
+  uint32_t length = 0;
+  uint32_t tmp = 0;
+  if (value < 0)
+  {
+    *(buffer++) = '-';
+    value *= -1;
+  }
+  do
+  {
+    stack[length] = '0' + (value % 10);
+    value /= 10;
+    length++;
+  } while (value);
+
+  tmp = length;
+  while (length)
+  {
+    *buffer = stack[length - 1];
+    length--;
+    buffer++;
+  }
+  return tmp;
 }
 #endif
