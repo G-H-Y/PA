c9079862000dafe5c3f396b6f49232870fcfe1ef
tracer-ics2018 <tracer@njuics.org>
2019-01-08 17:44:09 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  17:44:09 up  2:51,  1 user,  load average: 0.50, 0.75, 0.73 5a8eb94213446f479dd4458cdea395aa9bebb6ce

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,8 +5,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-
-  TODO();
+  __asm__ volatile("push %0"::"m"(ret_addr));
+  rtl_li(&decoding.seq_eip,vaddr_read(cpu.ldtr.base + NO,4));
+  //TODO();
 }
 
 void dev_raise_intr() {
