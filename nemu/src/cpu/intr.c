40ef220dab71f74906b9b13112c7cfaec70f2136
tracer-ics2018 <tracer@njuics.org>
2019-01-08 18:32:14 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  18:32:14 up  3:39,  1 user,  load average: 0.53, 0.73, 0.89 32a6c91c6170cf583123415801ea37847e6242d

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   rtl_push(&ret_addr);
-  rtl_li(&decoding.seq_eip,vaddr_read(cpu.ldtr.base + NO,4));
+  rtl_li(&decoding.seq_eip,vaddr_read(cpu.ldtr.base + (NO << 3),4));
   printf("in raise_intr: seq_eip = 0x%x, +NO = %d\n",decoding.seq_eip,cpu.ldtr.base + NO);
   //TODO();
 }
