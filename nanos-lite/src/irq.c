d49c2aa901d1db3df858f5589036aafe3ffc32c1
tracer-ics2018 <tracer@njuics.org>
2019-01-08 23:38:53 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:38:53 up  8:45,  1 user,  load average: 0.92, 1.00, 0.95 729752d717553a2e82cb549c421405b71b0ae525

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,11 +2,11 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: panic("event ID = %d",e.event);
+    case _EVENT_YIELD: panic("event ID = %d",e.event); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
-  return c;
+  return NULL;
 }
 
 void init_irq(void) {
