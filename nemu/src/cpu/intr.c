b8dec9661851559eade73162c89bb2f2b7b4694a
tracer-ics2018 <tracer@njuics.org>
2019-01-08 18:27:27 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  18:27:27 up  3:34,  1 user,  load average: 0.95, 1.17, 1.06 c334ef1ea7859ea1a4e703904032d16933f62a03

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,7 +7,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
   rtl_push(&ret_addr);
   rtl_li(&decoding.seq_eip,vaddr_read(cpu.ldtr.base + NO,4));
-  printf("in raise_intr: seq_eip = 0x%x, NO = %d\n",decoding.seq_eip,NO);
+  printf("in raise_intr: seq_eip = 0x%x, +NO = %d\n",decoding.seq_eip,cpu.ldtr.base + NO);
   //TODO();
 }
 
