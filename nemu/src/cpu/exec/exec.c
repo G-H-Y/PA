7c4a2a56cad7e06d7d2b7d69b8ecf9aa6d621977
tracer-ics2018 <tracer@njuics.org>
2019-01-09 18:21:55 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  18:21:55 up  9:02,  1 user,  load average: 1.96, 2.23, 1.73 2640f006433fea7b335f520b33436c2a2db971b2

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -240,11 +240,9 @@ void exec_wrapper(bool print_flag) {
     printf("in exec: leave exec_real\n");
 
 #ifdef DEBUG
-
   int instr_len = decoding.seq_eip - ori_eip;
+  printf("DEBUG instr_len = %d\n",instr_len);
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
-   if(decoding.seq_eip == 0x100a47)
-    printf("in exec: DEBUG\n");
   strcat(decoding.asm_buf, decoding.assembly);
   Log_write("%s\n", decoding.asm_buf);
   if (print_flag) {
