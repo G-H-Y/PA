13ecf7eb846b7d27f5661241af60994b768efc31
tracer-ics2018 <tracer@njuics.org>
2019-01-09 16:06:37 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:06:37 up  6:47,  1 user,  load average: 1.19, 1.23, 1.15 cd54b32141d25694c435352d3ecd9f97d1d4c81a

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,15 +5,17 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
-  printf("now in int\n");
+ 
   rtl_push(&cpu.eflags.val);
   rtl_push(&cpu.cs);
   rtl_li(&t0,ret_addr);
   rtl_push(&t0);
+  printf("now read vaddr\n");
   rtl_li(&t0,vaddr_read(cpu.ldtr.base + (NO << 3),4) & 0x0000ffff);
   rtl_li(&t1,vaddr_read(cpu.ldtr.base + (NO << 3) + 4,4) & 0xffff0000);
   rtl_or(&t2,&t0,&t1);
   rtl_li(&decoding.seq_eip,t2);
+   printf("now leave int\n");
   //printf("in raise_intr: seq_eip = 0x%x, +NO = %d\n",decoding.seq_eip,cpu.ldtr.base + NO);
   //TODO();
 }
