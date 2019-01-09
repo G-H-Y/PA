f85c9f3885d0f0d2a9efd64a071853dba8dcff53
tracer-ics2018 <tracer@njuics.org>
2019-01-09 16:05:02 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  16:05:02 up  6:45,  1 user,  load average: 0.63, 1.02, 1.07 4b380fad3e78b8573c2b37886d6d84075eab4f7

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,6 +5,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
+  printf("now in int\n");
   rtl_push(&cpu.eflags.val);
   rtl_push(&cpu.cs);
   rtl_li(&t0,ret_addr);
