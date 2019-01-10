3a35c763ebce0b4f0dcc01562524cdd3d4133bee
tracer-ics2018 <tracer@njuics.org>
2019-01-10 13:01:40 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:01:40 up  3:24,  1 user,  load average: 2.02, 1.98, 1.31 317d1726a0c51a738489d7ebe657a4d67fabfea

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,7 +34,8 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   //_exit(SYS_write);
-  int ret = _syscall_(SYS_write, (intptr_t)fd, (intptr_t)buf, (intptr_t)count);
+  printf("in _write: begin to syscall\n");
+  int ret = _syscall_(SYS_write, (uintptr_t)fd, (uintptr_t)buf, (uintptr_t)count);
   return ret;
 }
 
