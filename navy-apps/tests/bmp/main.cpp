da3502697574475563aa94119944555fc6ec781b
tracer-ics2018 <tracer@njuics.org>
2019-01-11 23:37:49 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:37:49 up  6:09,  1 user,  load average: 1.40, 1.66, 1.43 9ae6a46d8428d8c8773bbe6acc75797c7cfe5cf1

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -9,7 +9,9 @@ int main() {
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
   printf("in main:pixels\n");
   assert(bmp->pixels);
+  printf("after assert\n");
   NDL_OpenDisplay(bmp->w, bmp->h);
+  printf("after OPENDISP\n");
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
   NDL_Render();
   NDL_CloseDisplay();
