473e2ceb6862f8ff1fa609332930f27998a884ef
tracer-ics2018 <tracer@njuics.org>
2019-01-11 13:01:03 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:01:03 up 54 min,  1 user,  load average: 0.21, 0.32, 0.41 80274f4cfbe911c57b54839968352b904fee6aa6

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,17 +68,19 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
   if (fd>= FD_FB)
   {
-    Log("file size = %d,len = %d",file_table[fd].size,len);
+    //Log("file size = %d,len = %d",file_table[fd].size,len);
+    Log("before read: openoffset = %d",file_table[fd].open_offset);
     size_t disk_offset = file_table[fd].disk_offset;
     size_t open_offset = file_table[fd].open_offset;
     size_t fsize = file_table[fd].size;
     size_t aval_size = fsize - open_offset ;
-    Log("aval_size = %d",aval_size);
+    //Log("aval_size = %d",aval_size);
     size_t read_len = (aval_size > len) ? len : aval_size;
-    Log("offset = %d,read_len = %d",disk_offset + open_offset,read_len);
+    //Log("offset = %d,read_len = %d",disk_offset + open_offset,read_len);
     len = ramdisk_read(buf, disk_offset + open_offset, read_len);
+    Log("read len = %d",len);
     fs_lseek(fd, open_offset + len, SEEK_SET);
-    Log("openoffset = %d, len = %d",file_table[fd].open_offset,len);
+    Log("after read: openoffset = %d",file_table[fd].open_offset);
     return ((open_offset + read_len) == fsize) ? 0 : read_len;
   }
   return -1;
