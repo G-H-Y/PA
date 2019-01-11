984868ac8c894e216ed034f279da1944289fb628
tracer-ics2018 <tracer@njuics.org>
2019-01-11 12:55:36 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:55:36 up 48 min,  1 user,  load average: 0.34, 0.40, 0.47 a036601fcdee46fed1756af1c9d9d6f0c15ca153

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,16 +68,17 @@ size_t fs_read(int fd, void *buf, size_t len)
 {
   if (fd>= FD_FB)
   {
-    Log("file size = %d",file_table[fd].size);
+    Log("file size = %d,len = %d",file_table[fd].size,len);
     size_t disk_offset = file_table[fd].disk_offset;
     size_t open_offset = file_table[fd].open_offset;
     size_t fsize = file_table[fd].size;
     size_t aval_size = fsize - open_offset ;
+    Log("aval_size = %d",aval_size);
     size_t read_len = (aval_size > len) ? len : aval_size;
     Log("offset = %d,read_len = %d",disk_offset + open_offset,read_len);
-    size_t len = ramdisk_read(buf, disk_offset + open_offset, read_len);
+    len = ramdisk_read(buf, disk_offset + open_offset, read_len);
     fs_lseek(fd, open_offset + len, SEEK_SET);
-    Log("openoffset = %d",file_table[fd].size);
+    Log("openoffset = %d, len = %d",file_table[fd].open_offset,len);
     return ((open_offset + read_len) == fsize) ? 0 : read_len;
   }
   return -1;
