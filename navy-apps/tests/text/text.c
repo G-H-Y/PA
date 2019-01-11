2ba5b09ef9dbfeff87dcc22e00effae16f5c5c59
tracer-ics2018 <tracer@njuics.org>
2019-01-11 12:27:43 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:27:43 up 20 min,  1 user,  load average: 0.54, 0.78, 0.81 45645a93648e95564f1b113bf05269cad3d23a6a

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -4,20 +4,25 @@
 int main() {
   printf("in text main\n");
   FILE *fp = fopen("/share/texts/num", "r+");
+  
   assert(fp);
+  printf("assert(fp)\n");
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
+  printf("assert(size == 5000)\n");
 
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
+  printf("begin first for\n");
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
+  printf("begin second for\n");
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
