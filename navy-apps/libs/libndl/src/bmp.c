e0571048bb12b305810bf1ed80cf685474eddc35
tracer-ics2018 <tracer@njuics.org>
2019-01-11 22:13:01 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:13:01 up  4:44,  1 user,  load average: 0.94, 0.85, 0.85 9baca99c300643347184279717fc9ca11d1e1374

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -33,7 +33,7 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
 
   if (hdr.bitcount != 24) return -1;
   if (hdr.compression != 0) return -1;
-   printf("height= %d,width = %d\n",hdr.height,hdr.width);
+   printf("height= %d,width = %d, h*w*4 = %d\n",hdr.height,hdr.width,128*128*4);
   pixels = (uint32_t*)malloc(hdr.width * hdr.height * sizeof(uint32_t));
  
   if (!pixels) return -1;
