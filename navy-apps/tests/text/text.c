077ad5224ad127691e7dae96389018fb35ddc323
tracer-ics2018 <tracer@njuics.org>
2019-01-11 13:42:53 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:42:53 up  1:36,  1 user,  load average: 0.54, 0.58, 0.59 62758f8e941dbdbb3b2f994171e7dea6de5b91e3

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main() {
- // printf("in text main\n");
+int main()
+{
+  // printf("in text main\n");
   FILE *fp = fopen("/share/texts/num", "r+");
-  
+
   assert(fp);
- // printf("assert(fp)\n");
+  // printf("assert(fp)\n");
 
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
- /// printf("assert(size == 5000)\n");
+  /// printf("assert(size == 5000)\n");
 
-  //fseek(fp, 500 * 5, SEEK_SET);
-  fseek(fp,0,SEEK_SET);
+  fseek(fp, 500 * 5, SEEK_SET);
+  //fseek(fp, 0, SEEK_SET);
   int i, n;
- // printf("begin first for\n");
- // for (i = 500; i < 1000; i ++) 
-    for(i = 0; i<1000;i++)
+  // printf("begin first for\n");
+  for (i = 500; i < 1000; i++)
+  // for(i = 0; i<1000;i++)
   {
     //
     fscanf(fp, "%d", &n);
-    printf("i = %d,n = %d\n",i,n);
+    //printf("i = %d,n = %d\n", i, n);
     assert(n == i + 1);
   }
   printf("end first for\n");
 
   fseek(fp, 0, SEEK_SET);
   printf("begin second for\n");
-  for (i = 0; i < 500; i ++) {
+  for (i = 0; i < 500; i++)
+  {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  for (i = 500; i < 1000; i ++) {
+  for (i = 500; i < 1000; i++)
+  {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
-  for (i = 0; i < 500; i ++) {
+  for (i = 0; i < 500; i++)
+  {
     fscanf(fp, "%d", &n);
     assert(n == i + 1 + 1000);
   }
