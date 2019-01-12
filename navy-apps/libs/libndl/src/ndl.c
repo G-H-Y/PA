422f55355af0de9d1b295fbb1b4c3de754add428
tracer-ics2018 <tracer@njuics.org>
2019-01-12 10:57:18 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  10:57:18 up 17:28,  1 user,  load average: 1.13, 0.79, 0.81 376ce72163a89f7fff047f6b95b95f8bcc8f3fc4

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -52,12 +52,13 @@ int NDL_CloseDisplay() {
 }
 
 int NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
+  
   if (has_nwm) {
     for (int i = 0; i < h; i ++) {
-      printf("in DR:first for\n");
+      //printf("in DR:first for\n");
       printf("\033[X%d;%d", x, y + i);
       for (int j = 0; j < w; j ++) {
-        printf("in DR: second for\n");
+        //printf("in DR: second for\n");
         putchar(';');
         fwrite(&pixels[i * w + j], 1, 4, stdout);
       }
@@ -65,9 +66,11 @@ int NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
     }
   } else {
     for (int i = 0; i < h; i ++) {
-      printf("in DR:3th for\n");
+     // printf("in DR:3th for\n");
       for (int j = 0; j < w; j ++) {
-        printf("in DR:4th for\n");
+        //printf("in DR:4th for\n");
+        printf("in DR: x = %d,y = %d,w = %d, h = %d\n",x,y,w,h);
+        printf("j = %d,w = %d",j,w);
         canvas[(i + y) * canvas_w + (j + x)] = pixels[i * w + j];
       }
     }
