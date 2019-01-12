7d41e2c38b9b634dc8349181e09ec7149537a459
tracer-ics2018 <tracer@njuics.org>
2019-01-12 19:48:51 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  19:48:51 up  6:48,  1 user,  load average: 0.81, 0.66, 0.57 c055c6ef68ee4f6710c500821336706f34ca468

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -77,7 +77,7 @@ opcode_entry opcode_table [512] = {
   /* 0x08 */	IDEXW(G2E,or,1), IDEX(G2E,or), IDEXW(E2G,or,1), IDEX(E2G,or),
   /* 0x0c */	EMPTY, IDEX(I2a,or), EMPTY, EX(2byte_esc),
   /* 0x10 */	IDEXW(G2E,adc,1), EMPTY, EMPTY, IDEX(E2G,adc),
-  /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x14 */	IDEXW(I2a,adc,1), EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, IDEX(G2E,sbb), EMPTY, IDEX(E2G,sbb),
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x20 */	IDEXW(G2E,and,1), IDEX(G2E,and), IDEXW(E2G,and,1), IDEX(E2G,and),
