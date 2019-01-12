b6f68f7854a4e2784996b581b44e2818fb5e1b83
tracer-ics2018 <tracer@njuics.org>
2019-01-12 13:32:50 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:32:50 up 32 min,  1 user,  load average: 0.63, 0.96, 1.18 5676b8b43a5d7eae2c3d6be6ae40cc7a5f598d6

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -34,6 +34,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   //return 0;
+  Log("len = %d",len);
   int x = (offset / 4) % screen_width();
   int y = (offset / 4) / screen_height();
   int w = len / 4;
