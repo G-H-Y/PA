ed08bd1b30409c1dc72e38cd37c317bb6c1ef471
tracer-ics2018 <tracer@njuics.org>
2019-01-12 23:15:00 +0800

    >  compile U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  23:15:00 up 10:15,  1 user,  load average: 0.78, 0.59, 0.46 4ab6414bc4c0e4fa4bc113461fa0b83358698a5f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -3,23 +3,26 @@
 
 #define PMEM_SIZE (128 * 1024 * 1024)
 
-#define pmem_rw(addr, type) *(type *)({\
-    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
-    guest_to_host(addr); \
-    })
+#define pmem_rw(addr, type) *(type *)({                                       \
+  Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
+  guest_to_host(addr);                                                        \
+})
 
 uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
-bool is_diff_page(paddr_t addr,int len){
+bool is_diff_page(paddr_t addr, int len)
+{
   paddr_t page_offset = addr & 0xfff;
   paddr_t max_offset = 0xfff - len;
-  if(page_offset > max_offset) return true;
+  if (page_offset > max_offset)
+    return true;
   return false;
 }
 
-paddr_t page_translate(paddr_t addr){
+paddr_t page_translate(paddr_t addr)
+{
   paddr_t direct_entry = cpu.cr3 + ((addr >> 22) << 2);
   assert(direct_entry & 0x1);
   paddr_t page_addr = direct_entry & 0xfffff000;
@@ -30,36 +33,50 @@ paddr_t page_translate(paddr_t addr){
   return real_addr;
 }
 
-uint32_t paddr_read(paddr_t addr, int len) {
+uint32_t paddr_read(paddr_t addr, int len)
+{
   int map_NO = is_mmio(addr);
-  if(map_NO == -1){
-    assert(is_diff_page(addr,len));
+  if (map_NO == -1)
+  {
+    if (is_diff_page(addr, len))
+    {
+      Log("addr = 0x%x,len = %d",addr,len);
+      assert(0);
+    }
     paddr_t phy_addr = page_translate(addr);
     return pmem_rw(phy_addr, uint32_t) & (~0u >> ((4 - len) << 3));
   }
   else
   {
-    return mmio_read(addr,len,map_NO);
+    return mmio_read(addr, len, map_NO);
   }
-    
 }
 
-void paddr_write(paddr_t addr, uint32_t data, int len) {
+void paddr_write(paddr_t addr, uint32_t data, int len)
+{
   int map_NO = is_mmio(addr);
-  if(map_NO == -1){
-    assert(is_diff_page(addr,len));
+  if (map_NO == -1)
+  {
+    if (is_diff_page(addr, len))
+    {
+      Log("addr = 0x%x,len = %d",addr,len);
+      assert(0);
+    }
     paddr_t phy_addr = page_translate(addr);
     memcpy(guest_to_host(phy_addr), &data, len);
   }
-  else{
-    mmio_write(addr,len,data,map_NO);
-  }   
+  else
+  {
+    mmio_write(addr, len, data, map_NO);
+  }
 }
 
-uint32_t vaddr_read(vaddr_t addr, int len) {
+uint32_t vaddr_read(vaddr_t addr, int len)
+{
   return paddr_read(addr, len);
 }
 
-void vaddr_write(vaddr_t addr, uint32_t data, int len) {
+void vaddr_write(vaddr_t addr, uint32_t data, int len)
+{
   paddr_write(addr, data, len);
 }
