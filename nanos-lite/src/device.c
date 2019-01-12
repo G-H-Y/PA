15ff51f48099cd7a395a4190fd79d95143aacaad
tracer-ics2018 <tracer@njuics.org>
2019-01-12 13:30:43 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  13:30:43 up 30 min,  1 user,  load average: 0.84, 1.14, 1.27 f338e8bca923b4086fea434bf66cf32e92a03c44

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,7 +37,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int x = (offset / 4) % screen_width();
   int y = (offset / 4) / screen_height();
   int w = len / 4;
-  int h = 4;
+  int h = 2;
   draw_rect((unsigned int*)buf,x,y,w,h);
   return len;
 }
