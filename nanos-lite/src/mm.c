b20d534c70bd12a54333f2e3c75a7ee1820124bb
tracer-ics2018 <tracer@njuics.org>
2019-01-13 12:45:29 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:45:29 up  3:31,  1 user,  load average: 2.66, 1.12, 0.72 4e1ae04dd9b04c8d91f308cdb1fd3120f4b1a05e

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -21,7 +21,7 @@ int mm_brk(uintptr_t new_brk) {
 
 void init_mm() {
   pf = (void *)PGROUNDUP((uintptr_t)_heap.start);
-  Log("free physical pages starting from %d", pf);
+  Log("free physical pages starting from %x", pf);
 
   _vme_init(new_page, free_page);
   Log("end");
