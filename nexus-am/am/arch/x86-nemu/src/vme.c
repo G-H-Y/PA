607486da00319f403a4efc886839ede12ba403cb
tracer-ics2018 <tracer@njuics.org>
2019-01-14 00:28:52 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  00:28:52 up 15:15,  1 user,  load average: 1.01, 0.91, 0.96 c8ddad88c31417775baec994590674772b1b1589

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -97,6 +97,8 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
     printf("in map: ptr[offset] = %d\n",ptr[pd_offset]);
   }
   else {
+    pd_entry = pd_entry >> 1;
+    pd_entry = pd_entry << 1;
     printf("lsllall\n");
   }
   uint32_t pt_offset = (((uintptr_t)va >> 12) &(0x3ff)) << 2;
