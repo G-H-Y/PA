7c7ac73e8e594f9258c8a5d9f9a825b3e1aa6fce
tracer-ics2018 <tracer@njuics.org>
2019-01-13 22:48:23 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  22:48:23 up 13:34,  1 user,  load average: 0.40, 0.64, 0.76 c5d498dbcd269e04793838a3b8914c2c569ad6f0

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -85,11 +85,10 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
   printf("in map: ptr = %d\n",(uint32_t)ptr);
   printf("in map: ptr+offset = %d\n",(uint32_t)ptr+pd_offset);
   uint32_t pd_entry = ptr[pd_offset];
-  //printf("pd_entry = %d",pd_entry);
+  printf("pd_entry = %d",pd_entry);
   if(pd_entry == kpdirs[pd_offset]){
     PDE *uptable = (PDE*)(pgalloc_usr(1));
-    //ptr[pd_offset] = (uintptr_t)uptable | PTE_P;
-    ptr[pd_offset] = (uintptr_t)uptable &0x1;
+    ptr[pd_offset] = (uintptr_t)uptable | PTE_P;
     pd_entry = ptr[pd_offset];
   }
   printf("in map: ptr[offset] = %d\n",ptr[pd_offset]);
