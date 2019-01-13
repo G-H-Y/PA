f46683eb417d2730124995c9c7f55165906d5bf5
tracer-ics2018 <tracer@njuics.org>
2019-01-13 20:07:11 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  20:07:11 up 10:53,  1 user,  load average: 0.40, 0.52, 0.55 8289eff745ab5c74def7a8e8196ff0fb8b6277f7

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -43,7 +43,7 @@ int _vme_init(void* (*pgalloc_f)(size_t), void (*pgfree_f)(void*)) {
       }
     }
   }
-  //printf("start set cr\n");
+  printf("start set cr3 = %d\n",(int)kpdirs);
   set_cr3(kpdirs);
   set_cr0(get_cr0() | CR0_PG);
   //printf("in vem init: have set cr0\n");
