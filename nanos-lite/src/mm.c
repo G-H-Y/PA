42492c55d48626ef81dbab0c55a3d3e9decef389
tracer-ics2018 <tracer@njuics.org>
2019-01-14 12:27:15 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  12:27:15 up  2:07,  1 user,  load average: 0.74, 0.95, 0.96 c189e403630342552b3b075bf9cd87ab77dbb95

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -18,7 +18,7 @@ void free_page(void *p) {
 }
 
 /* The brk() system call handler. */
-/*int mm_brk(uintptr_t new_brk) {
+int mm_brk(uintptr_t new_brk) {
   printf("in mmbrk\n");
   //need a new page?
   printf("newbrk = %d, maxbrk = %d\n",new_brk,current->max_brk);
@@ -37,9 +37,9 @@ void free_page(void *p) {
     }
   }
   return 0;
-}*/
+}
 
-int mm_brk(uintptr_t new_brk) {
+/*int mm_brk(uintptr_t new_brk) {
   // if(new_brk > max_brk){
   if(current->max_brk > new_brk){
     printf("max_brk %x is bigger than new_brk %x\n", current->max_brk, new_brk);
@@ -51,7 +51,7 @@ int mm_brk(uintptr_t new_brk) {
     _map(&(current->as), (void *)(current->max_brk), pa, 0);
   }
   return 0;
-}
+}*/
 
 
 void init_mm() {
