a47745f794208c5ba28e31566a4ff64182d4fee3
tracer-ics2018 <tracer@njuics.org>
2019-01-14 14:44:27 +0800

    >  run U201514563 ghannah Linux ghannah-PC 4.15.0-29deepin-generic #31 SMP Fri Jul 27 07:12:08 UTC 2018 x86_64 GNU/Linux  14:44:27 up  4:24,  1 user,  load average: 1.60, 1.65, 1.23 6f8a7b99caebd143144a0caf9160510839607b1e

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -11,44 +11,50 @@ static PCB pcb[MAX_NR_PROC] __attribute__((used));
 static PCB pcb_boot;
 PCB *current;
 
-void switch_boot_pcb() {
+void switch_boot_pcb()
+{
   current = &pcb_boot;
 }
 
-void hello_fun(void *arg) {
+void hello_fun(void *arg)
+{
   int j = 1;
-  while (1) {
+  while (1)
+  {
     Log("Hello World from Nanos-lite for the %dth time!", j);
-    j ++;
+    j++;
     _yield();
   }
 }
 
-void init_proc() {
+void init_proc()
+{
   Log("in init_proc");
   //naive_uload(NULL, "/bin/init");
-  context_uload(&pcb[0],"/bin/hello");
+  context_uload(&pcb[0], "/bin/hello");
   context_uload(&pcb[1], "/bin/pal");
   //Log("uload pal end");
   //Log("pcb[0]->as->ptr = %d",pcb[1].as.ptr);
- 
-//Log("pcb[0]->as->ptr = %d",pcb[0].as.ptr);
+
+  //Log("pcb[0]->as->ptr = %d",pcb[0].as.ptr);
   //Log("uload hello end");
   switch_boot_pcb();
 }
 
 static uint32_t cnt = 0;
-_Context* schedule(_Context *prev) {
+_Context *schedule(_Context *prev)
+{
   current->cp = prev;
- cnt++;
-// current = &pcb[1];
- 
- current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
-// if((current == &pcb[0])&&(cnt % 100)){
-//   current = &pcb[1];
-// }
- //current = (current == &pcb[1]) ? &pcb[0] : &pcb[1];
- /*if(current == &pcb[1]){
+  cnt++;
+  // current = &pcb[1];
+
+  current = (current == &pcb[0]) ? &pcb[1] : &pcb[0];
+  if ((current == &pcb[0]) && (cnt % 100))
+  {
+    current = &pcb[1];
+  }
+  //current = (current == &pcb[1]) ? &pcb[0] : &pcb[1];
+  /*if(current == &pcb[1]){
    printf("begin pal!\n");
  }
  if(current == &pcb[0])
